b30fe1066bf2d90c5bd9994b319a2eaca4aa72ce
Anthony Nguyen <anthonyanthonyanthonynguyen@gmail.com>
2024-11-03 01:38:22 -0700

    fixed syntax error XD... also implemented the whole webring

--- makeWidget.js
+++ makeWidget.js
@@ -1,3 +1,4 @@
+// By default, the webring fetches from this LINK, but you can fetch a local .json if wanted :)
 const DATA = `https://raw.githubusercontent.com/toeeeee/uci-webring/main/webring.json`;
 
 // Widget stuff
@@ -28,23 +29,34 @@ class WebRing extends HTMLElement {
         const thisSite = this.getAttribute("site");
 
         fetch(DATA)
-            .then((response) => response.json())
+            .then((response) => {
+                if (!response.ok) { throw new Error("not working, see:" + response.status); }
+                return response.json();
+            })
             .then((sites) => {
                 // Current site find in JSON
                 const matchedSiteI = sites.findIndex(
-                    (site) => site.url === thisSite
+                    (site) => {
+                        console.log(site);
+                        return (site.url === thisSite);
+                    }
                 );
 
                 const matchedSite = sites[matchedSiteI];
 
-                let prevSiteI = matchedSiteI-1; 
+                let prevSiteI = matchedSiteI - 1; 
                 if (prevSiteI === -1) prevSiteI = sites.length - 1; // Wrap around
 
-                let nextSiteI = matchedSite+1;
+                let nextSiteI = matchedSiteI+1;
                 if (nextSiteI >= sites.length) nextSiteI = 0;
 
                 const randomSiteI = this.getRandomInt(0, sites.length-1);
-                
+                while (randomSiteI === matchedSiteI) { randomSiteI = this.getRandomInt(0, sites.length-1); }
+
+                const prevSite = sites[prevSiteI];
+                const nextSite = sites[nextSiteI];
+                const randomSite = sites[randomSiteI];
+
                 // MODIFY HTML HERE TO ADD IMAGES, ETC
                 const cp = `
                 <h1> Zot zot zot! </h1>
